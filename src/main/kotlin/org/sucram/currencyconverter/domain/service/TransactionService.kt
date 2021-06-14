7635d120ddbb7b7b1da3d45aa643bb4ebf431d01
Marcus Cruz <marcus.cruz85@gmail.com>
2021-06-13 22:08:52 -0300

    add transaction service

--- src/main/kotlin/org/sucram/currencyconverter/domain/service/TransactionService.kt
+++ src/main/kotlin/org/sucram/currencyconverter/domain/service/TransactionService.kt
@@ -1,4 +1,36 @@
 package org.sucram.currencyconverter.domain.service
 
-class TransactionService {
+import TransactionRepository
+import org.slf4j.LoggerFactory
+import org.sucram.currencyconverter.domain.Transaction
+import org.sucram.currencyconverter.services.ExchangeService
+import org.sucram.currencyconverter.web.controllers.dto.ConversionDto
+
+class TransactionService(private val exchangeService: ExchangeService, private val transactionRepository: TransactionRepository) {
+
+    private val logger = LoggerFactory.getLogger(this::class.java.name)
+
+    fun convert(conversionDto: ConversionDto): Transaction {
+        logger.info("convert $conversionDto")
+
+        val conversion = exchangeService.convert(conversionDto.from, conversionDto.to, conversionDto.amount);
+
+        val transaction = Transaction(
+            userId = conversionDto.userId,
+            originCurrency = conversionDto.from,
+            originAmount = conversionDto.amount,
+            destinationCurrency = conversionDto.to,
+            destinationAmount = conversion.result,
+            exchangeRate = conversion.rate
+        )
+
+        val transactionId = transactionRepository.create(transaction)
+        return transaction.copy(id = transactionId)
+    }
+
+
+    fun findByUser(userId: Long):List<Transaction> {
+        return transactionRepository.findByUserId(userId)
+    }
 }
+
