2be3e4f7bcfefe1e9ca05f4d22097f8542b6c6d4
Marcus Cruz <marcus.cruz85@gmail.com>
2021-06-14 02:14:40 -0300

    add transaction controller tests

---
+++ src/test/kotlin/org/sucram/currencyconverter/domain/service/ExchangeServiceTest.kt
@@ -0,0 +1,23 @@
+package org.sucram.currencyconverter.domain.service
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.sucram.currencyconverter.api.ExchangeRatesAPIService
+import org.sucram.currencyconverter.services.ExchangeService
+
+
+class ExchangeServiceTest {
+
+    @Mock
+    lateinit var exchangeRatesAPIService: ExchangeRatesAPIService
+
+    @Test
+    fun doAction_doesSomething(){
+
+    }
+
+}
+
---
+++ src/test/kotlin/org/sucram/currencyconverter/web/controllers/TransactionControllerTest.kt
@@ -0,0 +1,103 @@
+package org.sucram.currencyconverter.web.controllers
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.javalin.Javalin
+import org.eclipse.jetty.http.HttpStatus
+import org.junit.AfterClass
+import org.junit.Assert.assertEquals
+import org.junit.BeforeClass
+import org.junit.Test
+import org.sucram.currencyconverter.config.AppConfig
+import org.sucram.currencyconverter.domain.Transaction
+import org.sucram.currencyconverter.util.HttpUtil
+import java.io.File
+
+class TransactionControllerTest {
+
+
+    companion object {
+
+        @JvmStatic
+        private lateinit var app: Javalin
+
+        @JvmStatic
+        private lateinit var http: HttpUtil
+
+        @BeforeClass
+        @JvmStatic
+        fun start() {
+            app =  AppConfig().setup().start();
+            http = HttpUtil(app.port())
+
+            val file = File("src/test/resources/request_brl_to_usd.json")
+            val fileContent = file.readText()
+            val jsonFile: JsonNode = ObjectMapper().readTree(fileContent)
+
+            http.createTransaction(jsonFile)
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun stop() {
+            app.stop()
+        }
+    }
+
+    @Test
+    fun `get all transactions from user id`() {
+
+        // Arrange
+        val userId = 1000
+
+        // Act
+        val response = http.get<List<Transaction>>("/users/$userId/transactions")
+
+        // Assert
+        assertEquals(response.status, HttpStatus.OK_200)
+        assertEquals(response.body?.size, 1)
+        assertEquals(response.body?.get(0)?.userId, userId)
+        assertEquals(response.body?.get(0)?.originCurrency, "BRL")
+        assertEquals(response.body?.get(0)?.originAmount, 5.0)
+        assertEquals(response.body?.get(0)?.destinationCurrency, "USD")
+    }
+
+
+    @Test
+    fun `get status code 400 from wrong currency symbol`() {
+
+        // Arrange
+        val file = File("src/test/resources/request_wrog_symbol.json")
+        val fileContent = file.readText()
+        val jsonFile: JsonNode = ObjectMapper().readTree(fileContent)
+
+
+        // Act
+        val response = http.createTransaction(jsonFile)
+
+        // Assert
+        assertEquals(response.status, HttpStatus.BAD_REQUEST_400)
+
+
+    }
+
+    @Test
+    fun `success convert currency`() {
+
+        // Arrange
+        val file = File("src/test/resources/request_brl_to_usd.json")
+        val fileContent = file.readText()
+        val jsonFile: JsonNode = ObjectMapper().readTree(fileContent)
+
+        // Act
+        val response = http.createTransaction(jsonFile)
+
+        // Assert
+        assertEquals(response.status, HttpStatus.CREATED_201)
+
+    }
+
+
+
+
+}
