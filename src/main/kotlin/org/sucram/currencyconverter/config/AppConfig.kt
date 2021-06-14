1811e6a668bfe1f2c85b12a9e67ec4dcd5c12434
Marcus Cruz <marcus.cruz85@gmail.com>
2021-06-13 23:23:18 -0300

    Add open api configuration files

--- src/main/kotlin/org/sucram/currencyconverter/config/AppConfig.kt
+++ src/main/kotlin/org/sucram/currencyconverter/config/AppConfig.kt
@@ -1,6 +1,9 @@
 package org.sucram.currencyconverter.config
 
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.javalin.Javalin
+import io.javalin.plugin.json.JavalinJackson
 import org.eclipse.jetty.server.Server
 import org.koin.core.KoinProperties
 import org.koin.standalone.KoinComponent
@@ -8,6 +11,7 @@ import org.koin.standalone.StandAloneContext
 import org.koin.standalone.inject
 import org.sucram.currencyconverter.web.controllers.Router
 import org.sucram.currencyconverter.web.controllers.TransactionController
+import java.text.SimpleDateFormat
 
 class AppConfig: KoinComponent {
 
@@ -22,20 +26,36 @@ class AppConfig: KoinComponent {
             KoinProperties(true, true)
         )
 
+        this.configureMapper()
         val port: Int = System.getenv("PORT")?.toIntOrNull() ?: 7000
         val app = Javalin.create { config ->
             config.apply {
                 enableWebjars()
                 enableCorsForAllOrigins()
+                addStaticFiles("/swagger")
+                addSinglePageRoot("","/swagger/swagger-ui.html")
                 server {
                     Server(port)
                 }
             }
+        }.events {
+            it.serverStopping {
+                StandAloneContext.stopKoin()
+            }
         }
 
        router.register(app)
-
         return app
+    }
+
+    private fun configureMapper() {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        JavalinJackson.configure(
+            jacksonObjectMapper()
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .setDateFormat(dateFormat)
+                .configure(SerializationFeature.WRITE_DATES_WITH_ZONE_ID, true)
+        )
     }
 
 }
